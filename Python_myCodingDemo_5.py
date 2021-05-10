7193d1c7c70f30392dbdcf8c22818e0eb5986f8b
Stevan Villegas <83602597+stevanvillegas@users.noreply.github.com>
2021-05-10 18:22:52 -0400

    Update Python_myCodingDemo_5.py

--- Python_myCodingDemo_5.py
+++ Python_myCodingDemo_5.py
@@ -1,44 +1,60 @@
-# the assignment was to expand on the Python_myCodingDemo_3 program
+# the assignment was to expand on the 'Python_myCodingDemo_2' program; error handling
 
+employee_id = 1
+employees = []
 
+# prompt the user to say how many employees they will add
+# using error handling (try/except, else) to repeat the prompt until an integer is entered (while loop)
+while True:
+    try: 
+        num = int(input("Enter the number of employees: "))
+    except:
+        print("Please enter number of employees")
+    else:
+        break
 
-# each employee is a dictionary like this : 
-#       "name" : "john William"
-#       "age" : 40
+# loop (while) for each employee and record their information
+while employee_id <= num:
+    full_name = input("Enter employee's first and last name: ").split(" ")
+ 
+    first_name = full_name[0].lower().capitalize()
+    last_name = full_name[1].lower().capitalize()
 
-try:
-    num = int(input("Number of employees: "))
-    if num > 100:           # max number of employees is 100
-        raise Exception
-    except Exception:
-        print("Try Again")
-        done = False
-        while done is False:
-            buffer = input("Number of employees: ")
-            if buffer.isdigit():
-                num = int(buffer)
-                if num <= 100:
-                    done = True
-
-employeelist = []
-
-for i in range(num):
-    try:
-        print(f"Enter employee information: ")
-        employee = dict.fromkeys(["name", "age"])
-        employee["name"] = input("Employee name: ")
-        employee["age"] = int(input("Employee age: "))
-        if employee["age"] < 18:
-            raise Exception
+# use error handling (try/except(multi-except), else) to repeat the prompt until an integer is entered for age
+    while True:
+        try:
+            age_input = int(input("Enter employee's age: "))
+ # raise an error if the employee is under 18
+            if age_input < 18:
+                raise Exception
+        except ValueError:
+            print("please enter a number for age")
         except Exception:
-            print("Try Again")
-            done = False
-            while done is False:
-                buffer = input("Employee age: ")
-                if buffer.isdigit():
-                    if int(buffer) >= 18:
-                        employee["age"] = int(buffer)
-                        done = True
+            print("Age too low")
+        else:
+            break
+    
+    email = f"{first_name.lower()}.{last_name.lower()}{employee_id}@company.com"
+
+    employee = {
+        "first_name": first_name,
+        "last_name": last_name,
+        "age": age_input,
+        "email": email
+    }
+    employees.append(employee)
+    employee_id += 1
+
+# print each employee's information in a formatted string
+for employee in employees:
+    print(f""first name: {employee["first_name"]}
+             last name: {employee["last_name"]}
+             age: {employee["age"]}
+             email: {employee["email"]}"")
+                # if buffer.isdigit():
+                    # if int(buffer) >= 18:
+                        # employee["age"] = int(buffer)
+                        # done = True
 
     finally:
         employee.append(employee)
