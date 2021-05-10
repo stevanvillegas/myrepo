1e6a3c7d254394e892529554630426e8db882d74
Stevan Villegas <83602597+stevanvillegas@users.noreply.github.com>
2021-05-10 16:38:14 -0400

    Update Python_myNotes.py

--- Python_myNotes.py
+++ Python_myNotes.py
@@ -1,33 +1,5 @@
 # This is documentation of my Cognixia Python learning experience:
 
-# This is a comment
-"""
-This is a multi line string
-    The spacing is preserved
-This is how multi line comments are created in Python
-"""
-#Sample exercise:
-print("Hello World") # This is a print statement
-
-print(42)
-
-print([1,2,3])
-
-print(40 + 2)
-
-name = input("Enter your name: ")
-print(name)
-
-if name == "Hello World":
-    print("You entered " + name)
-
-if name == "42":
-            print("You entered 42")
-            print("That is a number")
-            print("This will not work")
-     
-    
-    
 # This is a list:
 fruits = ["apple", "mango", "banana", "kiwi", "peach"]
 print(fruits)
