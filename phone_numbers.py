8fdc8b0e1768826ee13f32372455810ff30cbaf9
Stevan Villegas <83602597+stevanvillegas@users.noreply.github.com>
2021-05-10 18:25:39 -0400

    Add files via upload

---
+++ phone_numbers.py
@@ -0,0 +1,7 @@
+phone = input("Enter a phone number to see if it is valid: ")
+phone_digits = phone.replace("(","").replace(")","").replace("-","").replace(" ","")
+
+
+if ((len(phone_digits) != 10) or ("911" in phone_digits)):
+    print(f"Phone Number {phone} is invalid")
+else: print(f"Phone Number {phone} is valid")
