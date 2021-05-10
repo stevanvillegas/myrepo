f295287004abddd6334d0ed5a458b1ce87b912cd
Stevan Villegas <83602597+stevanvillegas@users.noreply.github.com>
2021-05-10 17:36:11 -0400

    Update and rename fizzbuzz.py to Python_myCodingDemo_4.py

--- Python_myCodingDemo_4.py
+++ Python_myCodingDemo_4.py
@@ -1,13 +1,20 @@
+# the assignment was to create a program that accepts a user's integer input
+
 num = int(input("Enter an integer: "))
 num_list = [0]
 
+# range(start, stop) 
 for i in range(1, num +1):
+# if the number is divisible by both 3 and 5, add "Fizzbuzz"
     if (i % 15 ==0 ):
         num_list.append("Fizzbuzz")
+# if the number is divisible by 3, add "Fizz"
     elif i % 3 == 0:
         num_list.append("Fizz")
+# if the number is divisible by 5, add "Buzz"
     elif i % 5 == 0:
         num_list.append("Buzz")
+# if the number is divisible by neither, add the number itself
     else:
         num_list.append(i)
 
